e9a39a6d9ec2f6da2c158d9f963b95e269910046
tjnoun <tjnoun@gmail.com>
2023-04-18 19:10:28 -0500

    19.1 activities

---
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,19 @@
+// Create the addNumbers function here.
+function addNumbers() {
+    var two=2;
+    var three=3; 
+    var five= two + three;
+
+    addNumbers(five)
+}
+
+// Create the stringConcat function here.
+function stringConcat() {
+    var taco='taco';
+    var cat='cat';
+    var tacoCat = taco + ' ' + cat;
+
+    alert(tacoCat);
+}
+
+stringConcat();
